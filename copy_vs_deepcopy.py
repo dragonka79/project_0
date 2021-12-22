443ce70bcdf7bc79a6cd3ea9c8be47c23f48cd8d
dragonka79 <dragonka79@gmail.com>
2021-12-22 11:01:34 +0100

    main

--- copy_vs_deepcopy.py
+++ copy_vs_deepcopy.py
@@ -1,19 +1,20 @@
+
 from copy import copy
 from copy import deepcopy
 
 xs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 # Shallow copy
-# ys= copy(xs)
+ys = copy(xs)
 
-# xs.append("új_copy")
-# xs[2][2] = 0
-# print(f"xs: {xs}")
-# print(f"ys: {ys}")
+xs.append("új_copy")
+xs[2][2] = 0
+print(f"xs: {xs}")
+print(f"ys: {ys}")
 
 # Deepcopy
 
-zs= deepcopy(xs)
+zs = deepcopy(xs)
 
 xs.append("új_deepcopy")
 xs[2][2] = 1
