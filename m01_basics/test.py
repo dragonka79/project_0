47cfe00d991f446e4b022a0b22644315dd4e7ce7
dragonka79 <dragonka79@gmail.com>
2022-01-20 05:05:16 +0100

    add list comprehension with conditional

--- m01_basics/test.py
+++ m01_basics/test.py
@@ -12,3 +12,14 @@ print("list1:",list1)
 list2 = [i for i in range(1,5)]
 print("list2:", list2)
 
+# List comprehension way with conditional
+# Listing the even numbers between [2,10]
+
+list_even = [i for i in range(1,11) if i%2 ==0]
+print("list_even:", list_even)
+
+# List comprehension way with conditional
+# Listing the odd numbers between [1,9]
+
+list_odd = [i for i in range(1,11) if i%2 !=0]
+print("list_odd:", list_odd)
