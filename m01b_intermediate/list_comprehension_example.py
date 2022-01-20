53eff478b430e8cc19317cf07d39bfa51365c1de
dragonka79 <dragonka79@gmail.com>
2022-01-20 05:15:09 +0100

    added list comprehension with conditional

--- m01b_intermediate/list_comprehension_example.py
+++ m01b_intermediate/list_comprehension_example.py
@@ -1,3 +1,4 @@
+
 # Non list comprehension way
 
 list1 = list()
@@ -10,3 +11,15 @@ print("list1:",list1)
 
 list2 = [i for i in range(1,5)]
 print("list2:", list2)
+
+# List comprehension way with conditional
+# Listing the even numbers between [2,10]
+
+list_even = [i for i in range(1,11) if i%2 ==0]
+print("list_even:", list_even)
+
+# List comprehension way with conditional
+# Listing the even numbers between (1,18) except the ones cannot be divided by 4.
+
+list_even_except_4 = [i for i in range(1,18) if ( i%2 ==0 and not i%4==0)]
+print("list_even_except_4:", list_even_except_4)
