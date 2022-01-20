3b4b9ef5990731a3865a40493ebd522cdcbb529f
dragonka79 <dragonka79@gmail.com>
2022-01-20 06:05:34 +0100

    no changes

--- m01b_intermediate/l_13_normalize_data.py
+++ m01b_intermediate/l_13_normalize_data.py
@@ -77,6 +77,7 @@ ip_addr_1 = "10.0.1.1"
 ip_addr_2 = "10.000.001.001"
 ip_addr_3 = "010.00.01.001"
 
+
 if IPv4Address(ip_addr_1) == IPv4Address(ip_addr_2) == IPv4Address(ip_addr_3):
     print("--IP address normalization works")
 else:
