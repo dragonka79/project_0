20776cdd34f2bbb1fe2360247dcc5b895ff8e17b
dragonka79 <dragonka79@gmail.com>
2022-01-23 15:44:53 +0100

    main

--- m01_basics/test.py
+++ m01_basics/test.py
@@ -1,11 +1,38 @@
-# Can a x number be divided by 13?
-x = 18
-divide13 = lambda x: x%13==0
-print(divide13(x))
+# {
+#     "workbench.colorTheme": "Dark-Dracula",
+#     "explorer.confirmDragAndDrop": false,
+#     "explorer.confirmDelete": false,
+#     "workbench.sideBar.location": "right",
+#     "git.confirmSync": false,
+#     "terminal.integrated.scrollback": 100000,
+#     "workbench.colorCustomizations": {
+#         "tab.activeBackground": "#008080",
+#     },
+#     "editor.minimap.enabled": false,
+#     "editor.rename.enablePreview": false,
+#     "editor.rulers": [80]
+# }
 
-# Can a y number be divided by 17?
-y = 18
-def divide17(y):
-    return y%17==0
 
-print(divide17(y))
+
+
+
+class Pont:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def sulypont_szamitas(self, masik_pont):
+        """ A súlypontom a másik ponttal. """
+        mx = (self.x + masik_pont.x) / 2
+        my = (self.y + masik_pont.y) / 2
+        return (mx, my)
+
+p = Pont(3, 4)
+q = Pont(5, 12)
+r = p.sulypont_szamitas(q)
+print(r)
+
+# Ua mint két sorral fentebb, csak beszédesebb kimenettel
+print(f"A két pont súlypontja: {Pont(3, 4).sulypont_szamitas(Pont(5, 12))}")
+
