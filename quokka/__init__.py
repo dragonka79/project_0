7bd5798d33379658b9d78125df54082c8a788be4
dragonka79 <dragonka79@gmail.com>
2022-01-31 17:02:05 +0100

    main

--- quokka/__init__.py
+++ quokka/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import yaml
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<p>Hello, zaultschy!</p>"
+def import_devices():
+    with open ("quokka/data/devices.yaml") as devices_file:
+        devices = yaml.safe_load(devices_file.read())
+        return devices
+
+
+@app.route("/devices/")
+def devices():
+    devices = import_devices
+    return {'devices': devices}
