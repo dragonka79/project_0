ab5a973e5a1beda66e5e2772532984ae65dd5bae
dragonka79 <dragonka79@gmail.com>
2022-02-01 17:24:44 +0100

    main

--- quokka/__init__.py
+++ quokka/__init__.py
@@ -1,23 +1,33 @@
-# from flask import Flask
-# import yaml
-
-# app = Flask(__name__)
-
-# def import_devices():
-#     with open ("/quokka/data/devices.yaml") as devices_file:
-#         devices = yaml.safe_load(devices_file.read())
-#         return devices
-
-
-# @app.route("/devices/")
-# def devices():
-#     devices = import_devices
-#     return {'devices': devices}
-
 from flask import Flask
+import yaml
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_zaultschy():
-    return "<p>Hello, zaultschy!</p>"
+def import_devices():
+    with open ("quokka/data/devices.yaml") as devices_file:
+        devices = yaml.safe_load(devices_file.read())
+        return devices
+
+
+@app.route("/devices/")
+def devices():
+    devices = import_devices()
+    return {'devices': devices}
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/device/")
+# def hello_world():
+#     device = {
+#         "name": "sbx-n9kv-ao",
+#         "vendor": "cisco",
+#         "model": "Nexus9000 C9300c Chassis",
+#         "os": "nxos",
+#         "version": "9.3(3)",
+#         "ip": "10.1.1.1",
+
+#     }
+#     return device
+    # return "<p>Hello, zaultschy!</p>"
