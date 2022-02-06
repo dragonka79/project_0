dd4c2e55f88a44d38600438d58d0ac27515ecb26
dragonka79 <dragonka79@gmail.com>
2022-02-06 19:23:29 +0100

    main

--- daproject/capture/capture_pyshark.py
+++ daproject/capture/capture_pyshark.py
@@ -8,7 +8,8 @@ def print_packet_summary(pkt):
 # CAPTURE EVERYTHING AND PRINT PACKET SUMMARIES
 print("\n\t\t\t+++ Packet summaries +++\n")
 capture = pyshark.LiveCapture(interface='ens33', only_summaries=True)
-capture.sniff(packet_count=5) # It starts to capture here
+capture.sniff(packet_count=5) # It starts to capture here, waits until the 
+# all the packets counted arrives
 for packet in capture:
     print_packet_summary(packet)
 
@@ -22,6 +23,7 @@ for packet in capture:
     print_packet_summary(packet)
 
 # CAPTURE AND HANDLE HTTPS PACKETS 
+# After a packet arrives it is handled
 # iterate through the packets, passing in a function to apply to each packet
 print("\n\t\t\t+++ Print packets as they are detected +++\n")
 capture = pyshark.LiveCapture(interface='ens33', only_summaries=True, 
