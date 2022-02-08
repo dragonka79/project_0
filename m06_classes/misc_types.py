c95062aec4584af5a28af525346aa901a7e418b1
dragonka79 <dragonka79@gmail.com>
2022-02-08 07:07:51 +0100

    main

--- m06_classes/misc_types.py
+++ m06_classes/misc_types.py
@@ -7,6 +7,6 @@ class TransportType:
 class DeviceType:
     IOS = "ios"
     NXOS = "nxos"
-    NXOS_SSH = "nxos_ssh"
+    NXOS_SSH = "nxos_ssh" # Only for NAPALM
     NEXUS = "nexus"
     CISCO_NXOS = "cisco_nxos"
