eb2fb70c42828357e4e236a149ccd48039f3ea9b
dragonka79 <dragonka79@gmail.com>
2022-02-10 10:43:01 +0100

    main

---
+++ daproject/devices.py
@@ -0,0 +1,23 @@
+sandbox_devices = {
+    "ios": {
+        "hostname": "sandbox-iosxe-recomm-1.cisco.com",
+        "port": 22,
+        "username": "developer",
+        "password": "C1sco12345",
+        "device_type": "cisco_ios",
+    },
+    "iosxr": {
+        "hostname": "sandbox-iosxr-1.cisco.com",
+        "port": 22,
+        "username": "admin",
+        "password": "C1sco12345",
+        "device_type": "cisco_ios",
+    },
+    "nxos": {
+        "hostname": "sandbox-nxos-1.cisco.com",
+        "port": 443,
+        "username": "admin",
+        "password": "Admin_1234!",
+        "device_type": "cisco_nxos",
+    },
+}
